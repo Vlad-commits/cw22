52838bd81d59ff257b94ddfaa13d6f5a33f3d205
Vlad Mamaev <st011079@student.spbu.ru>
2020-04-05 14:29:38 +0300

    add plot functions for PDF and histograms

--- plots.py
+++ plots.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_cdf_and_ecdfs(samples, cdf):
-    X = np.linspace(-8, 5, 10000)
+def plot_cdf_and_ecdfs(samples, cdf, left=-8, right=5):
+    X = np.linspace(left, right, 10000)
     plt.plot(X, [cdf(x) for x in X], label="real")
     for i, sample in enumerate(samples):
-        sns.distplot(sample, hist=False, label="sample " + str(i) + " histogram",
+        sns.distplot(sample, hist=False, label="sample " + str(i) + " ECDF",
                      kde_kws={'cumulative': True})
+
+
+def plot_pdf_and_histograms(samples, pdf, left=-8, right=5):
+    X = np.linspace(left, right, 10000)
+    plt.plot(X, [pdf(x) for x in X], label="real")
+    for i, sample in enumerate(samples):
+        sns.distplot(sample, hist=False, label="sample " + str(i) + " histogram")
